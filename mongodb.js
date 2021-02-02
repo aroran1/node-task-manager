ad714294b6ea4eaf612228f4a4a9da74b34366a1
aroran1 <nidhigulati2001@gmail.com>
2021-02-02 23:19:31 +0000

    created first documents and fields

--- mongodb.js
+++ mongodb.js
@@ -6,12 +6,20 @@
  const MongoClient = mongodb.MongoClient;
 
  const connectionUrl = 'mongodb://127.0.0.1:27017'; // use IP instead of localhost as it slows down the application
- const databaseName = 'task-manager';
+ const DATABASE_NAME = 'task-manager';
 
  // useNewUrlParser required to connect correctly useNewUrlParser
  mongodb.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
   if(error) {
     return console.log('Unable to connect to databse!');
   }
-  console.log('connected correctly!');
+
+  // takes 1 argument of you db name
+  const db = client.db(DATABASE_NAME)
+
+  // Run this and test it out in robo 3T to check the uopdate
+  db.collection('users').insertOne({
+    name: 'Nidhi',
+    age: 37
+  });
 }); 
