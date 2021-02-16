aad512e3862dc008f015fd716826adb7ba4b5de2
aroran1 <nidhigulati2001@gmail.com>
2021-02-16 12:58:00 +0000

    added async/await examples to mongoose

--- playground/promise-chaining-mongoose-task.js
+++ playground/promise-chaining-mongoose-task.js
@@ -3,11 +3,26 @@ const Task = require('../src/models/task');
 
 // 602968a63dff49c74816377f
 
-Task.findByIdAndDelete('6029c3091c9547da9b5e7ea9').then(task => {
-  console.log(task);
-  return Task.countDocuments({ completed: false})
-}).then(result => {
-  console.log(result);
+// promise chaining with return and then
+// Task.findByIdAndDelete('6029c3091c9547da9b5e7ea9').then(task => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false})
+// }).then(result => {
+//   console.log(result);
+// }).catch(e => {
+//   console.log(e);
+// });
+
+// promise chaining with async/await
+const deleteTaskAndCount = async (id) => {
+  // if you don't need to use the value of const task the you can run the method without storing it in a const
+  const task = await Task.findByIdAndDelete(id); 
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+}
+
+deleteTaskAndCount('60255307223ac7a4f99057c2').then(count => {
+  console.log(count);
 }).catch(e => {
   console.log(e);
-});
+})
--- playground/promise-chaining-mongoose-user.js
+++ playground/promise-chaining-mongoose-user.js
@@ -3,15 +3,16 @@ const User = require('../src/models/user');
 
 // 602963e5757693c526633db3
 
-User.findByIdAndUpdate('6029b9518d32a2d89b1fab94', { age: 1 }).then(user => {
-  console.log(user);
+// promise chaining with return and then
+// User.findByIdAndUpdate('6029b9518d32a2d89b1fab94', { age: 1 }).then(user => {
+//   console.log(user);
 
-  return User.countDocuments({ age: 1})
-}).then(result => {
-  console.log(result);
-}).catch(e => {
-  console.log(e);
-});
+//   return User.countDocuments({ age: 1})
+// }).then(result => {
+//   console.log(result);
+// }).catch(e => {
+//   console.log(e);
+// });
 
 // TERMINAL OUTPUT:
 // console.log(user); from line 7
@@ -34,3 +35,17 @@ User.findByIdAndUpdate('6029b9518d32a2d89b1fab94', { age: 1 }).then(user => {
 //   __v: 0
 // }
 // 2
+
+// promise chaining with async/await
+const updateAgeAndCount = async (id, age) => {
+  // if you don't need to use the value of const user the you can run the method without storing it in a const
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount('602968230edef6c707fc9701', 2).then(count => {
+  console.log(count);
+}).catch(e => {
+  console.log(e);
+})
